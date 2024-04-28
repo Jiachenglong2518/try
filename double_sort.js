0d4609f2288ff259afca65a70abbf4d7c1177f59
jiachenglong <jiacl@nancal.com>
2024-04-28 17:31:25 +0800

    feat: 格式化

--- double_sort.js
+++ double_sort.js
@@ -1,28 +1,32 @@
+//双重排序
+function sort_more(data) {
+  data.sort(function (a, b) {
+    if (a.index == b.index) {
+      return b.key - a.key;
+    } else {
+      return a.index - b.index;
+    }
+  });
+  console.log(data);
 
-      //双重排序
-      function sort_more(data) {
-        data.sort(function(a,b){
-          if(a.index == b.index){
-            return  b.key - a.key ;
-          } else {
-            return a.index - b.index; 
-          }
-        })
-        console.log(data);
-        
-        return data;
-      } 
-      var sort_arr = [{
-        index:0,
-        key:2
-      }, {
-        index:3,
-        key:1
-      }, {
-        index:1,
-        key:3
-      }, {
-        index:3,
-        key:2
-      }]
-      sort_more(sort_arr,'index', 'key');
+  return data;
+}
+var sort_arr = [
+  {
+    index: 0,
+    key: 2,
+  },
+  {
+    index: 3,
+    key: 1,
+  },
+  {
+    index: 1,
+    key: 3,
+  },
+  {
+    index: 3,
+    key: 2,
+  },
+];
+sort_more(sort_arr, "index", "key");
